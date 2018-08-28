d6e4a22a8f7fa3a33e4a9dcc781075c4d068d3a5
Garrett Fermo <garrettcfermo@gmail.com>
2018-08-28 09:57:29 -0700

    Created the Questions Object

---
+++ assets/javascript/game.js
@@ -0,0 +1,35 @@
+
+// Questions Object
+
+let questions = [
+  {
+    questions: 'On what Hawaiian island is the Banzai Pipeline located?',
+    choices: ['Hawwaii', 'Oahu','California','Maui'],
+    correct: 'Oahu'
+  },
+  {
+    questions: 'What do you call a surfer that rides with his right foot forward? ',
+    choices: ['Goofy','Reg','Normal','None of the Above'],
+    correct: 'Goofy'
+  },
+  {
+    questions: ' Of the four main surfboard types, which is traditionally the shortest?',
+    choices: ['Thruster','Single','Fish','Quad'],
+    correct: 'Fish'
+  },
+  {
+    questions: 'Cloud Nine is a gnarly break, but where is it?',
+    choices: ['California','Mexico','Phillippines','Bali'],
+    correct: 'Phillippines'
+  },
+  {
+    questions: 'What did Huntington Beach, CA become known as because of its influence on surfing?',
+    choices: ['Surf City' ,' Surf Captial','Surf City California','Surf City, USA'],
+    correct: 'Surf City, USA'
+  },
+  {
+    questions: "Rolling Stones is not only a rock and roll band it's also a surf break in which part of the world ?",
+    choices: ['New Zealand','London','Hawaii','Australia'],
+    correct:'New Zealand'
+  }
+]
