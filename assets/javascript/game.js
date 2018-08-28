b31529f111366dd2513c223d6c830020457576ff
Garrett Fermo <garrettcfermo@gmail.com>
2018-08-28 10:41:26 -0700

    JavaScript Creates HTML Questions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,33 +3,62 @@
 
 let questions = [
   {
-    questions: 'On what Hawaiian island is the Banzai Pipeline located?',
-    choices: ['Hawwaii', 'Oahu','California','Maui'],
+    question: 'On what Hawaiian island is the Banzai Pipeline located?',
+    choices: ['Hawwaii', 'Oahu', 'California', 'Maui'],
     correct: 'Oahu'
   },
   {
-    questions: 'What do you call a surfer that rides with his right foot forward? ',
-    choices: ['Goofy','Reg','Normal','None of the Above'],
+    question: 'What do you call a surfer that rides with his right foot forward? ',
+    choices: ['Goofy', 'Reg', 'Normal', 'None of the Above'],
     correct: 'Goofy'
   },
   {
-    questions: ' Of the four main surfboard types, which is traditionally the shortest?',
-    choices: ['Thruster','Single','Fish','Quad'],
+    question: ' Of the four main surfboard types, which is traditionally the shortest?',
+    choices: ['Thruster', 'Single', 'Fish', 'Quad'],
     correct: 'Fish'
   },
   {
-    questions: 'Cloud Nine is a gnarly break, but where is it?',
-    choices: ['California','Mexico','Phillippines','Bali'],
+    question: 'Cloud Nine is a gnarly break, but where is it?',
+    choices: ['California', 'Mexico', 'Phillippines', 'Bali'],
     correct: 'Phillippines'
   },
   {
-    questions: 'What did Huntington Beach, CA become known as because of its influence on surfing?',
-    choices: ['Surf City' ,' Surf Captial','Surf City California','Surf City, USA'],
+    question: 'What did Huntington Beach, CA become known as because of its influence on surfing?',
+    choices: ['Surf City', ' Surf Captial', 'Surf City California', 'Surf City, USA'],
     correct: 'Surf City, USA'
-  },
-  {
-    questions: "Rolling Stones is not only a rock and roll band it's also a surf break in which part of the world ?",
-    choices: ['New Zealand','London','Hawaii','Australia'],
-    correct:'New Zealand'
   }
 ]
+
+// Creates the HTML for Questions
+questions.forEach(question => {
+  $('.container').append(`
+    <div class="row">
+      <h5 class="question">${question.question}</h5>
+     <p>
+      <label>
+        <input name="group1" type="radio" checked />
+        <span>${question.choices[0]}</span>
+      </label>
+    </p>
+     <p>
+      <label>
+        <input name="group1" type="radio" checked />
+        <span>${question.choices[1]}</span>
+      </label>
+    </p>
+     <p>
+      <label>
+        <input name="group1" type="radio" checked />
+        <span>${question.choices[2]}</span>
+      </label>
+    </p>
+     <p>
+      <label>
+        <input name="group1" type="radio" checked />
+        <span>${question.choices[3]}</span>
+      </label>
+    </p>
+      <p class="answer">${question.correct}</p>
+    </div>
+  `)
+});
